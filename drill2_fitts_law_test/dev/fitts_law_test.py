6ba53f9e93971e8849d2536c083638b5748b374f
10freefingers <10freefingers@gmail.com>
2014-04-18 22:57:12 +0200

    Parse test setup with relative path

--- drill2_fitts_law_test/dev/fitts_law_test.py
+++ drill2_fitts_law_test/dev/fitts_law_test.py
@@ -7,20 +7,43 @@ from PyQt4 import QtGui, QtCore
 class ClickRecorder(QtGui.QWidget):
 
 
+
     def parseDescription(self):
 
         #read input
 	with open(self.fileName) as f:
         #with open(self.fileName) as f:
-            self.content = f.readlines()
+            content = f.read()
 
-        if not self.content:
+        if not content:
             print 'File is empty'
         
-        #self.userData = 
-	#circleWidths = content.
+	#split array of form
+	#USER:1
+	#WIDTHS:10,20,30,40
+	#DISTANCES:100,200,300,400
+	#USER:2...
+	#to structure
+	#{1 : { widths : [10,20,30,40] }, { distances: [100,200..] } }
+
+        #split content into array with infos of the user
+        usersData = content.split('(User)')
         
-        print self.content
+        setupData = {}
+        for userData in usersData:
+	    userLines = userData.split()
+	    
+   	    userId = userLines[0].split(':')[1]
+	    widths = map(int, userLines[1].split(':')[1].split(','))
+	    distances = map(int, userLines[2].split(':')[1].split(','))
+	    
+            setupData.update({"userId" : 
+            {
+                "widths" : widths,
+                "distances" : distances
+	    }})
+
+        print setupData
 
     def __init__(self):
         super(ClickRecorder, self).__init__()
@@ -28,10 +51,18 @@ class ClickRecorder(QtGui.QWidget):
         self.initUI()
         self.requestFileName()
         self.parseDescription()
+	#self.initTestSetup()
+
+    #def initTestSetup(self):
+        
+        #self.testSetup = 
+        #{
+        #    "1": [
+        #}
 
     def requestFileName(self):
         #self.fileName = raw_input("Please enter the file name including path containing descriptions for this test.")
-        self.fileName = "/home/michl/Desktop/sf_SharedLinuxFolder/Git/interactiondesign-python/data/test_description.txt"
+        self.fileName = "../test/test_setup.txt"
         
     def initUI(self):
         self.text = "Circle click test"
