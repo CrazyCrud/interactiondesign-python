049efaf2d6774045b218b8b8284b4f11bf3f5ab3
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-04-22 12:32:39 +0200

    Modified main.py

--- drill2_fitts_law_test/dev/main.py
+++ drill2_fitts_law_test/dev/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import csv
 import datetime
 import random
@@ -39,7 +40,9 @@ class ClickRecorder(QtGui.QWidget):
 		self.widths 	= map(int, userLines[1].split(':')[1].split(','))
 		self.distances 	= map(int, userLines[2].split(':')[1].split(','))
 
-		#randomize width and distance arrays order 
+		self.filename = "user" + self.userId
+
+		#randomize width and distance arrays order
 		random.shuffle(self.widths)
 		random.shuffle(self.distances)
 
@@ -57,10 +60,13 @@ class ClickRecorder(QtGui.QWidget):
 
 
 	def setupLogging(self):
-		with open("user1.csv", "a+") as logfile:
-			#!! delete or make array global?
-			output = csv.DictWriter(logfile, ["UserID", "Width", "Distance", "Timestamp", "Movements", "Errors"])
-			output.writeheader()
+		if os.path.exists(self.filename + ".csv"):
+			return
+		else:
+			with open(self.filename + ".csv", "a") as logfile:
+				output = csv.DictWriter(logfile, ["UserID", "Width", "Distance", "Timestamp", "Movements", "Errors"],
+					delimiter=';')
+				output.writeheader()
 
 
 	def setupCircles(self):
@@ -74,14 +80,14 @@ class ClickRecorder(QtGui.QWidget):
 		width 		= self.widths[self.trialsCount]
 		distance 	= self.distances[self.trialsCount]
 
-		marginleft = width
+		marginleft = int((1100 - (distance)) / 2)
 
 		#create circle and add to current two-element circle array
 		self.circles.append(Circle(width, marginleft, constHeight))
 		self.circles.append(Circle(width, marginleft + distance, constHeight))
 
 
-	def setupTimer(self): 
+	def setupTimer(self):
 		self.timer = QtCore.QTimer()
 		self.timer.timeout.connect(self.timeUp)
 
@@ -90,12 +96,15 @@ class ClickRecorder(QtGui.QWidget):
 		self.movements = 0
 		self.errors = 0
 		self.trialsCount += 1
+		self.isReady = False
+
 
 	def initUser(self):
 		self.trialsCount = 0
 
+
 	def initUI(self):
-		self.setGeometry(0, 0, 1100, 300)
+		self.setGeometry(0, 0, 1100, 400)
 		self.setWindowTitle("A Study about Fitts's Law")
 		self.setFocusPolicy(QtCore.Qt.StrongFocus)
 		self.center()
@@ -115,29 +124,37 @@ class ClickRecorder(QtGui.QWidget):
 		qp.setBrush(QtGui.QColor(255, 255, 255))
 		qp.drawRect(event.rect())
 		self.drawCircles(event, qp)
+		self.drawState(event, qp)
 		qp.end()
 
 
 	def mousePressEvent(self, event):
 		if event.button() == QtCore.Qt.LeftButton:
-			if self.timer.isActive == False:
+			if self.isReady == False and not self.timer.isActive():
+				return
+
+			if self.timer.isActive() == False:
+				print "Start timer"
 				self.timer.start(1000 * 60)
 
 			for circle in self.circles:
 				if circle.isClicked(event.pos()) == True:
 					if self.currentCircle == None:
+						print "Start position"
 						self.currentCircle = circle
 						return
 					elif circle != self.currentCircle:
+						print "Correct start to target movement"
 						self.currentCircle = circle
 						self.movements += 1
 						return
 					else:
-						self.currentCircle = None
+						self.currentCircle = circle
 						self.errors += 1
 						return
 
 			if self.currentCircle != None:
+				print "Click out of boundaries"
 				self.errors += 1
 
 			self.currentCircle = None
@@ -148,27 +165,48 @@ class ClickRecorder(QtGui.QWidget):
 			circle.drawCircle(event, qp)
 
 
+	def drawState(self, event, qp):
+		qp.setPen(QtGui.QColor(80, 80, 80))
+		qp.setFont(QtGui.QFont('Decorative', 24))
+		if self.isReady:
+			qp.drawText(event.rect(), QtCore.Qt.AlignBottom, "Ready")
+		else:
+			qp.drawText(event.rect(), QtCore.Qt.AlignBottom, "Not ready")
+
+
+	def keyPressEvent(self, ev):
+		if ev.key() == QtCore.Qt.Key_Space:
+			if self.timer.isActive():
+				return
+			else:
+				self.isReady = not self.isReady
+				self.update()
+
+
 	def timeUp(self):
+		print "Time is up"
 		self.timer.stop()
 		self.logResults()
 		self.resetTrial()
 
 
 	def resetTrial(self):
+		self.isReady = False
 		self.movements = 0
 		self.errors = 0
 		self.setupCircles()
+		self.update()
 
 
 	def logResults(self):
-		with open("user1.csv", "a") as logfile:
+		with open(self.filename + ".csv", "a") as logfile:
 			timestamp = datetime.datetime.now().strftime("%I:%M%p")
 			data = {
-				"UserID": self.userId, 
-				"Width": self.widths[self.trialsCount], 
-				"Distance": self.distances[self.trialsCount], 
-				"Timestamp": timestamp, 
-				"Movements": self.movements, 
+				"UserID": self.userId,
+				"Width": self.widths[self.trialsCount],
+				"Distance": self.distances[self.trialsCount],
+				"Timestamp": timestamp,
+				"Movements": self.movements,
 				"Errors": self.errors
 			}
 			output = csv.DictWriter(logfile, ["UserID", "Width", "Distance", "Timestamp", "Movements", "Errors"])
