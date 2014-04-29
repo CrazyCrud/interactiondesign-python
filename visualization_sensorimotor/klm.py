4bca3751062d16100cb832d1c857e722a3112aa0
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-04-29 15:50:58 +0200

    Added some functions

--- visualization_sensorimotor/klm.py
+++ visualization_sensorimotor/klm.py
@@ -1,2 +1,27 @@
 # -*- coding: utf-8 -*-
 
+
+import sys
+import re
+
+
+def main():
+    if(len(sys.argv) > 1):
+        with f as open(sys.argv[1], 'r'):
+            parse_file(f)
+    else:
+        pass
+
+
+def parse_file(file):
+    operators = []
+    for line in file:
+        operators.append(parse_line(line))
+
+
+def parse_line(line):
+    pass
+
+
+if __name__ = '__main__':
+    main()
