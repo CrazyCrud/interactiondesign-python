f96b51c2b36fb77b08abee1e0de9da6511bc0262
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-04-30 16:11:10 +0200

    Operators are extracted correclty

--- visualization_sensorimotor/klm.py
+++ visualization_sensorimotor/klm.py
@@ -7,7 +7,7 @@ import re
 
 def main():
     if(len(sys.argv) > 1):
-        with f as open(sys.argv[1], 'r'):
+        with open(sys.argv[1], 'r') as f:
             parse_file(f)
     else:
         pass
@@ -15,13 +15,39 @@ def main():
 
 def parse_file(file):
     operators = []
+    operators_map = {}
     for line in file:
-        operators.append(parse_line(line))
+        line = remove_comment(line)
+        if len(line) > 0:
+            operators = operators + parse_line(line)
+    for sequence in operators:
+        results = re.search(r'[0-9]+', sequence)
+        if results is not None:
+            count = (int)(results.group())
+        else:
+            count = 1
+
+        results = re.search(r'[a-zA-Z]+', sequence)
+        if results is not None:
+            operator = results.group()
+
+        if operator in operators_map:
+            operators_map[operator] = operators_map[operator] + count
+        else:
+            operators_map[operator] = count
+
+
+def remove_comment(line):
+    results = re.search(r'#', line)
+    if results is not None:
+        line = line[0:results.start()]
+    return line
 
 
 def parse_line(line):
-    pass
+    results = re.findall(r'[0-9]*[a-zA-Z]{1}', line)
+    return results
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
