264875274a6b1e1fa75f3c0ec1d23deb4f108365
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-05-29 11:21:14 +0200

    Filtered graphs

--- drill5_wiimote/analyze.py
+++ drill5_wiimote/analyze.py
@@ -42,23 +42,47 @@ class Demo(pyqtgraph.GraphicsLayoutWidget):
         self.showFullScreen()
         pyqtgraph.setConfigOptions(antialias=True)
 
-        self.x_plot = self.addPlot()
-        self.x_plot.setTitle("The X Accelerometer")
-        self.x_plot.setMenuEnabled(False)
-        self.x_plot.setClipToView(True)
-        self.x_plot.hideAxis('bottom')
+        self.x_plot_raw = self.addPlot()
+        self.x_plot_raw.setTitle("The X Accelerometer")
+        self.x_plot_raw.setMenuEnabled(False)
+        self.x_plot_raw.setClipToView(False)
+        self.x_plot_raw.hideAxis('bottom')
+        self.x_plot_raw.showGrid(x=True, y=True, alpha=0.5)
+        self.nextColumn()
+        self.x_plot_filtered = self.addPlot()
+        self.x_plot_filtered.setTitle("The X Accelerometer - Filtered")
+        self.x_plot_filtered.setMenuEnabled(False)
+        self.x_plot_filtered.setClipToView(False)
+        self.x_plot_filtered.hideAxis('bottom')
+        self.x_plot_filtered.showGrid(x=True, y=True, alpha=0.5)
         self.nextRow()
-        self.y_plot = self.addPlot()
-        self.y_plot.setTitle("The Y Accelerometer")
-        self.y_plot.setMenuEnabled(False)
-        self.y_plot.setClipToView(True)
-        self.y_plot.hideAxis('bottom')
+        self.y_plot_raw = self.addPlot()
+        self.y_plot_raw.setTitle("The Y Accelerometer")
+        self.y_plot_raw.setMenuEnabled(False)
+        self.y_plot_raw.setClipToView(False)
+        self.y_plot_raw.hideAxis('bottom')
+        self.y_plot_raw.showGrid(x=True, y=True, alpha=0.5)
+        self.nextColumn()
+        self.y_plot_filtered = self.addPlot()
+        self.y_plot_filtered.setTitle("The Y Accelerometer - Filtered")
+        self.y_plot_filtered.setMenuEnabled(False)
+        self.y_plot_filtered.setClipToView(False)
+        self.y_plot_filtered.hideAxis('bottom')
+        self.y_plot_filtered.showGrid(x=True, y=True, alpha=0.5)
         self.nextRow()
-        self.z_plot = self.addPlot()
-        self.z_plot.setTitle("The Z Accelerometer")
-        self.z_plot.setMenuEnabled(False)
-        self.z_plot.setClipToView(True)
-        self.z_plot.hideAxis('bottom')
+        self.z_plot_raw = self.addPlot()
+        self.z_plot_raw.setTitle("The Z Accelerometer")
+        self.z_plot_raw.setMenuEnabled(False)
+        self.z_plot_raw.setClipToView(False)
+        self.z_plot_raw.hideAxis('bottom')
+        self.z_plot_raw.showGrid(x=True, y=True, alpha=0.5)
+        self.nextColumn()
+        self.z_plot_filtered = self.addPlot()
+        self.z_plot_filtered.setTitle("The Z Accelerometer - Filtered")
+        self.z_plot_filtered.setMenuEnabled(False)
+        self.z_plot_filtered.setClipToView(False)
+        self.z_plot_filtered.hideAxis('bottom')
+        self.z_plot_filtered.showGrid(x=True, y=True, alpha=0.5)
 
     def add(self, x, y, z):
         self.node.add(x, y, z)
@@ -68,12 +92,9 @@ class Demo(pyqtgraph.GraphicsLayoutWidget):
         if values is None:
             return
         else:
-            self.x_plot.plot(numpy.random.normal(size=100), clear=True)
-            self.y_plot.plot(numpy.random.normal(size=100), clear=True)
-            self.z_plot.plot(numpy.random.normal(size=100), clear=True)
-            #self.x_plot.plot(values['x'], clear=True)
-            #self.y_plot.plot(values['y'], clear=True)
-            #self.z_plot.plot(values['z'], clear=True)
+            self.x_plot_raw.plot(values['x'], clear=True)
+            self.y_plot_raw.plot(values['y'], clear=True)
+            self.z_plot_raw.plot(values['z'], clear=True)
             pyqtgraph.QtGui.QApplication.processEvents()
 
     def keyPressEvent(self, ev):
@@ -83,26 +104,23 @@ class Demo(pyqtgraph.GraphicsLayoutWidget):
 
 class WiimoteNode(object):
     def __init__(self, n=100):
-        self.coordinates = {'x': [], 'y': [], 'z': []}
+        self.values = {'x': [], 'y': [], 'z': []}
         self.limit = n
 
     def add(self, x, y, z):
-        if len(self.coordinates['x']) > self.limit:
-            self.coordinates['x'].pop(0)
-            self.coordinates['y'].pop(0)
-            self.coordinates['z'].pop(0)
-        self.coordinates['x'].append(x)
-        self.coordinates['y'].append(y)
-        self.coordinates['z'].append(z)
+        if len(self.values['x']) > self.limit:
+            self.values['x'].pop(0)
+            self.values['y'].pop(0)
+            self.values['z'].pop(0)
+        self.values['x'].append(x)
+        self.values['y'].append(y)
+        self.values['z'].append(z)
 
     def get(self):
-        if len(self.coordinates['x']) == 0:
+        if len(self.values['x']) == 0:
             return None
         else:
-            x = self.coordinates['x'][len(self.coordinates['x']) - 1]
-            y = self.coordinates['y'][len(self.coordinates['y']) - 1]
-            z = self.coordinates['z'][len(self.coordinates['z']) - 1]
-            return {'x': x, 'y': y, 'z': z}
+            return self.values
 
 if __name__ == "__main__":
     main()
