b9de4fc0d08a63a1810b335b86ec4b33965ecfbc
10freefingers <10freefingers@gmail.com>
2014-06-01 12:18:20 +0200

    Overwrite old analyze.py

--- drill5_wiimote/analyze.py
+++ drill5_wiimote/analyze.py
@@ -1,8 +1,7 @@
 import wiimote
 import sys
 import time
-from random import randint
-import numpy
+#from random import randint
 from pyqtgraph.flowchart import Flowchart, Node
 import pyqtgraph.flowchart.library as fclib
 import pyqtgraph
@@ -15,10 +14,14 @@ def main():
     demo = Demo()
     demo.show()
 
+    # update wiimote data every few milliseconds
     while True:
         demo.updateValues(
-            wm.accelerometer._state[0], wm.accelerometer._state[1], wm.accelerometer._state[2])
-        time.sleep(0.10)
+            wm.accelerometer._state[0],
+            wm.accelerometer._state[1],
+            wm.accelerometer._state[2])
+            #randint(0, 6), randint(0, 6), randint(0, 6))
+        time.sleep(0.20)
 
     sys.exit(app.exec_())
 
@@ -46,8 +49,8 @@ class Demo(QtGui.QWidget):
         self.setWindowTitle("Plotting the Wiimote")
         self.showFullScreen()
 
-        layout = QtGui.QGridLayout()
-        self.setLayout(layout)
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
 
         self.flowchart = Flowchart(terminals={
             'xDataIn': {'io': 'in'},
@@ -58,135 +61,90 @@ class Demo(QtGui.QWidget):
             'zDataOut': {'io': 'out'}
         })
 
-        layout.addWidget(self.flowchart.widget(), 0, 0, 3, 1)
-
-        self.x_plot_raw = pyqtgraph.PlotWidget()
-        self.y_plot_raw = pyqtgraph.PlotWidget()
-        self.z_plot_raw = pyqtgraph.PlotWidget()
-        self.x_plot_filtered = pyqtgraph.PlotWidget()
-        self.y_plot_filtered = pyqtgraph.PlotWidget()
-        self.z_plot_filtered = pyqtgraph.PlotWidget()
-
-        #layout.addWidget(self.x_plot_raw, 0, 1, 1, 1)
-        layout.addWidget(self.x_plot_filtered, 0, 2, 1, 2)
-        #layout.addWidget(self.y_plot_raw, 1, 1, 1, 1)
-        layout.addWidget(self.y_plot_filtered, 1, 2, 1, 2)
-        #layout.addWidget(self.z_plot_raw, 2, 1, 1, 1)
-        layout.addWidget(self.z_plot_filtered, 2, 2, 1, 2)
-
-        pyqtgraph.setConfigOptions(antialias=True)
-
-        self.x_plot_raw.getPlotItem().setTitle("The X Accelerometer")
-        self.x_plot_raw.getPlotItem().setMenuEnabled(False)
-        self.x_plot_raw.getPlotItem().setClipToView(False)
-        self.x_plot_raw.getPlotItem().hideAxis('bottom')
-        self.x_plot_raw.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
-
-        self.x_plot_filtered.getPlotItem().setTitle(
-            "The X Accelerometer - Filtered")
-        self.x_plot_filtered.getPlotItem().setMenuEnabled(False)
-        self.x_plot_filtered.getPlotItem().setClipToView(False)
-        self.x_plot_filtered.getPlotItem().hideAxis('bottom')
-        self.x_plot_filtered.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
-
-        self.y_plot_raw.getPlotItem().setTitle("The Y Accelerometer")
-        self.y_plot_raw.getPlotItem().setMenuEnabled(False)
-        self.y_plot_raw.getPlotItem().setClipToView(False)
-        self.y_plot_raw.getPlotItem().hideAxis('bottom')
-        self.y_plot_raw.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
-
-        self.y_plot_filtered.getPlotItem().setTitle(
-            "The Y Accelerometer - Filtered")
-        self.y_plot_filtered.getPlotItem().setMenuEnabled(False)
-        self.y_plot_filtered.getPlotItem().setClipToView(False)
-        self.y_plot_filtered.getPlotItem().hideAxis('bottom')
-        self.y_plot_filtered.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
-
-        self.z_plot_raw.getPlotItem().setTitle("The Z Accelerometer")
-        self.z_plot_raw.getPlotItem().setMenuEnabled(False)
-        self.z_plot_raw.getPlotItem().setClipToView(False)
-        self.z_plot_raw.getPlotItem().hideAxis('bottom')
-        self.z_plot_raw.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
-
-        self.z_plot_filtered.getPlotItem().setTitle(
-            "The Z Accelerometer - Filtered")
-        self.z_plot_filtered.getPlotItem().setMenuEnabled(False)
-        self.z_plot_filtered.getPlotItem().setClipToView(False)
-        self.z_plot_filtered.getPlotItem().hideAxis('bottom')
-        self.z_plot_filtered.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
+        self.layout.addWidget(self.flowchart.widget(), 0, 0, 3, 1)
 
         fclib.registerNodeType(WiimoteNode, [('Display',)])
-        wii_node = self.flowchart.createNode('Wiimote', pos=(0, 0))
+        self.wii_node = self.flowchart.createNode('Wiimote', pos=(0, 0))
 
-        x_plot_raw_node = self.flowchart.createNode(
-            'PlotWidget', pos=(-450, -350))
-        x_plot_raw_node.setPlot(self.x_plot_raw)
-        x_plot_filtered_node = self.flowchart.createNode(
-            'PlotWidget', pos=(-300, -350))
-        x_plot_filtered_node.setPlot(self.x_plot_filtered)
+        self.axes = ['x', 'y', 'z']
 
-        y_plot_raw_node = self.flowchart.createNode(
-            'PlotWidget', pos=(-150, -350))
-        y_plot_raw_node.setPlot(self.y_plot_raw)
-        y_plot_filtered_node = self.flowchart.createNode(
-            'PlotWidget', pos=(0, -350))
-        y_plot_filtered_node.setPlot(self.y_plot_filtered)
+        # positions for all nodes. order:
+        # raw_node x, raw_node y, filtered_node x, filtered_node y,
+        # filter_node x, filter_node y
+        self.positions = {
+            'x': [-450, -350, -300, -350, -375, -150],
+            'y': [-150, -350, 0, -350, -75, -150],
+            'z': [150, -350, 300, -350, 225, -150],
+        }
 
-        z_plot_raw_node = self.flowchart.createNode(
-            'PlotWidget', pos=(150, -350))
-        z_plot_raw_node.setPlot(self.z_plot_raw)
-        z_plot_filtered_node = self.flowchart.createNode(
-            'PlotWidget', pos=(300, -350))
-        z_plot_filtered_node.setPlot(self.z_plot_filtered)
+        # create, style, config and connect the elements for every axis
+        for axis in self.axes:
+            index = self.axes.index(axis)
 
-        filter_x_node = self.flowchart.createNode(
-            'GaussianFilter', pos=(-375, -150))
-        filter_x_node.ctrls['sigma'].setValue(5)
-        filter_y_node = self.flowchart.createNode(
-            'GaussianFilter', pos=(-75, -150))
-        filter_y_node.ctrls['sigma'].setValue(5)
-        filter_z_node = self.flowchart.createNode(
-            'GaussianFilter', pos=(225, -150))
-        filter_z_node.ctrls['sigma'].setValue(5)
+            plot_raw = pyqtgraph.PlotWidget()
+            plot_filtered = pyqtgraph.PlotWidget()
+
+            # add widget for this axis in next row
+            self.layout.addWidget(plot_filtered, index, 2, 1, 2)
+
+            self.configPlotItems(axis, plot_raw, plot_filtered)
+
+            self.createNodes(axis, plot_raw, plot_filtered)
+
+            self.connectNodes(axis)
+
+        pyqtgraph.setConfigOptions(antialias=True)
 
         self.flowchart.setInput(xDataIn=0)
         self.flowchart.setInput(yDataIn=0)
         self.flowchart.setInput(zDataIn=0)
 
+    # create raw, filter and filtered node
+    def createNodes(self, axis, plot_raw, plot_filtered):
+
+        # create filtered node
+        self.plot_filtered_node = self.flowchart.createNode(
+            'PlotWidget', pos=(
+                self.positions[axis][2],
+                self.positions[axis][3]))
+        self.plot_filtered_node.setPlot(plot_filtered)
+
+        # create gaussian filter
+        self.filter_node = self.flowchart.createNode(
+            'GaussianFilter', pos=(
+                self.positions[axis][4],
+                self.positions[axis][5]))
+        self.filter_node.ctrls['sigma'].setValue(5)
+
+    # connect nodes: flowchart -> wiinode -> plot_raw +  filter_node
+    # -> filtered_node
+    def connectNodes(self, axis):
         self.flowchart.connectTerminals(
-            self.flowchart['xDataIn'], wii_node['xDataIn'])
-        self.flowchart.connectTerminals(
-            self.flowchart['yDataIn'], wii_node['yDataIn'])
-        self.flowchart.connectTerminals(
-            self.flowchart['zDataIn'], wii_node['zDataIn'])
+            self.flowchart[axis + 'DataIn'], self.wii_node[axis + 'DataIn'])
 
         self.flowchart.connectTerminals(
-            wii_node['xDataOut'], x_plot_raw_node['In'])
-        self.flowchart.connectTerminals(
-            wii_node['yDataOut'], y_plot_raw_node['In'])
-        self.flowchart.connectTerminals(
-            wii_node['zDataOut'], z_plot_raw_node['In'])
+            self.wii_node[axis + 'DataOut'], self.filter_node['In'])
 
         self.flowchart.connectTerminals(
-            wii_node['xDataOut'], filter_x_node['In'])
-        self.flowchart.connectTerminals(
-            wii_node['yDataOut'], filter_y_node['In'])
-        self.flowchart.connectTerminals(
-            wii_node['zDataOut'], filter_z_node['In'])
+            self.filter_node['Out'], self.plot_filtered_node['In'])
 
         self.flowchart.connectTerminals(
-            filter_x_node['Out'], x_plot_filtered_node['In'])
-        self.flowchart.connectTerminals(
-            filter_y_node['Out'], y_plot_filtered_node['In'])
-        self.flowchart.connectTerminals(
-            filter_z_node['Out'], z_plot_filtered_node['In'])
+            self.filter_node['Out'], self.flowchart[axis + 'DataOut'])
 
-        self.flowchart.connectTerminals(
-            filter_x_node['Out'], self.flowchart['xDataOut'])
-        self.flowchart.connectTerminals(
-            filter_y_node['Out'], self.flowchart['yDataOut'])
-        self.flowchart.connectTerminals(
-            filter_z_node['Out'], self.flowchart['zDataOut'])
+    # config plot items
+    def configPlotItems(self, axis, plot_raw, plot_filtered):
+        plot_raw.getPlotItem().setTitle("The " + axis + " Accelerometer")
+        plot_raw.getPlotItem().setMenuEnabled(False)
+        plot_raw.getPlotItem().setClipToView(False)
+        plot_raw.getPlotItem().hideAxis('bottom')
+        plot_raw.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
+
+        plot_filtered.getPlotItem().setTitle(
+            "The " + axis + " Accelerometer - Filtered")
+        plot_filtered.getPlotItem().setMenuEnabled(False)
+        plot_filtered.getPlotItem().setClipToView(False)
+        plot_filtered.getPlotItem().hideAxis('bottom')
+        plot_filtered.getPlotItem().showGrid(x=True, y=True, alpha=0.5)
 
     def updateValues(self, x, y, z):
         self.flowchart.setInput(xDataIn=x)
@@ -199,6 +157,7 @@ class Demo(QtGui.QWidget):
             self.close()
 
 
+# custom node for wiimote
 class WiimoteNode(Node):
     nodeName = 'Wiimote'
 
@@ -231,29 +190,5 @@ class WiimoteNode(Node):
         self.values['y'].append(y)
         self.values['z'].append(z)
 
-"""
-class WiimoteNode(object):
-    def __init__(self, n=100):
-        self.values = {'x': [], 'y': [], 'z': []}
-        self.limit = n
-
-    def add(self, x, y, z):
-        if len(self.values['x']) > self.limit:
-            self.values['x'].pop(0)
-            self.values['y'].pop(0)
-            self.values['z'].pop(0)
-        self.values['x'].append(x)
-        self.values['y'].append(y)
-        self.values['z'].append(z)
-
-    def getX(self):
-        return self.values['x']
-
-    def getY(self):
-        return self.values['y']
-
-    def getZ(self):
-        return self.values['z']
-"""
 if __name__ == "__main__":
     main()
