2ccd90c2326021e18e8107f238edfc9d0a2ae53e
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-06-18 16:39:07 +0200

    Added output to activity node

--- drill7_time_series/activity.py
+++ drill7_time_series/activity.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from pyqtgraph.flowchart import Flowchart
-from pyqtgraph.flowchart.library.common import CtrlNode, Node
+from pyqtgraph.flowchart.library.common import Node
 import pyqtgraph.flowchart.library as fclib
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
-from random import randint
 import sys
 import time
 
@@ -15,11 +14,10 @@ def main():
     demo = Demo()
     demo.show()
 
-    """
     while True:
         demo.update()
         time.sleep(0.20)
-    """
+
     sys.exit(app.exec_())
 
 
@@ -31,6 +29,7 @@ class ActivityNode(Node):
             'accelX': dict(io='in'),
             'accelY': dict(io='in'),
             'accelZ': dict(io='in'),
+            'activity': dict(io='out'),
         }
 
         Node.__init__(self, name, terminals=terminals)
@@ -39,12 +38,23 @@ class ActivityNode(Node):
         data_x_length = len(accelX)
         data_y_length = len(accelY)
         data_z_length = len(accelZ)
+
+        frequency_spectrum_x = np.fft.fft(accelX) / data_x_length
+        frequency_spectrum_x = frequency_spectrum_x[range(data_x_length / 2)]
+        frequency_spectrum_y = np.fft.fft(accelY) / data_y_length
+        frequency_spectrum_y = frequency_spectrum_y[range(data_y_length / 2)]
+        frequency_spectrum_z = np.fft.fft(accelZ) / data_z_length
+        frequency_spectrum_z = frequency_spectrum_z[range(data_z_length / 2)]
         """
-        frequency_spectrum = np.fft.fft(dataIn) / data_length # fft computing and normalization
+        frequency_spectrum = np.fft.fft(dataIn) / data_length
+            # fft computing and normalization
         frequency_spectrum = frequency_spectrum[range(data_length / 2)]
 
         output =  np.abs(frequency_spectrum)
         """
+
+        output = 'No activity yet...'
+        return {'activity': output}
 
 fclib.registerNodeType(ActivityNode, [('Data',)])
 
@@ -82,15 +92,13 @@ class Demo(QtGui.QWidget):
         self.layout.addWidget(pwX, 0, 1)
         self.layout.addWidget(pwY, 1, 1)
         self.layout.addWidget(pwZ, 2, 1)
-        self.layout.addWidget(self.label, 3,1)
+        self.layout.addWidget(self.label, 3, 1)
 
-        sampling_rate = 150.0;
-        sampling_interval = 1.0 / sampling_rate; # Abtastfrequenz f = (1/t)
+        sampling_rate = 150.0
+        sampling_interval = 1.0 / sampling_rate  # Abtastfrequenz f = (1/t)
         time_vector = np.arange(0, 1, sampling_interval)
-
-        signal_frequency = 10;
+        signal_frequency = 10
         data = np.sin(2 * np.pi * signal_frequency * time_vector)
-
         fc.setInput(dataIn=data)
 
         pwXNode = fc.createNode('PlotWidget', pos=(-150, -150))
@@ -102,8 +110,9 @@ class Demo(QtGui.QWidget):
         pwZNode = fc.createNode('PlotWidget', pos=(150, -150))
         pwZNode.setPlot(pwZ)
 
-        activityNode = fc.createNode('ActivityNode', pos=(0, 150))
+        self.activityNode = fc.createNode('ActivityNode', pos=(0, 150))
 
+        # Einkommentieren falls mit Wii gearbeitet wird
         """
         wiimoteNode = fc.createNode('Wiimote', pos=(-300, 0))
         bufferXNode = fc.createNode('Buffer', pos=(-150, 0))
@@ -111,15 +120,15 @@ class Demo(QtGui.QWidget):
         bufferZNode = fc.createNode('Buffer', pos=(150, 0))
         """
 
-        #dataX = dataY = dataZ = data
-
+        # Auskommentieren falls mit Wii gearbeitet wird
         fc.connectTerminals(fc['dataIn'], pwXNode['In'])
         fc.connectTerminals(fc['dataIn'], pwYNode['In'])
         fc.connectTerminals(fc['dataIn'], pwZNode['In'])
-        fc.connectTerminals(fc['dataIn'], activityNode['accelX'])
-        fc.connectTerminals(fc['dataIn'], activityNode['accelY'])
-        fc.connectTerminals(fc['dataIn'], activityNode['accelZ'])
+        fc.connectTerminals(fc['dataIn'], self.activityNode['accelX'])
+        fc.connectTerminals(fc['dataIn'], self.activityNode['accelY'])
+        fc.connectTerminals(fc['dataIn'], self.activityNode['accelZ'])
 
+        # Einkommentieren falls mit Wii gearbeitet wird
         """
         fc.connectTerminals(wiimoteNode['accelX'], bufferXNode['dataIn'])
         fc.connectTerminals(wiimoteNode['accelY'], bufferYNode['dataIn'])
@@ -133,7 +142,9 @@ class Demo(QtGui.QWidget):
         """
 
     def update(self):
-        pass
+        outputValues = self.activityNode.outputValues()
+        self.label.setText(outputValues['activity'])
+        pg.QtGui.QApplication.processEvents()
 
     def keyPressEvent(self, ev):
         if ev.key() == QtCore.Qt.Key_Escape:
