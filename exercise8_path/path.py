34ad2ad50ca1d6565ae35f0bc211fb4c673015aa
10freefingers <10freefingers@gmail.com>
2014-06-23 15:04:58 +0200

    Plotting infrared data in scatterplotitem

---
+++ exercise8_path/path.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+import time
+import sys
+from pyqtgraph.flowchart import Flowchart
+import pyqtgraph
+import pyqtgraph as pg
+from PyQt4 import QtGui, QtCore
+import wiimote
+from wiimote_node import *
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    demo = Demo()
+    demo.show()
+
+    while True:
+        demo.update()
+        time.sleep(0.05)
+
+    sys.exit(app.exec_())
+
+
+class Demo(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(Demo, self).__init__()
+
+        self.setWindowTitle("Pointing Device")
+        self.show()
+
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
+
+        self.fc = Flowchart(terminals={
+            'dataIn': {'io': 'in'},
+            'dataOut': {'io': 'out'}
+        })
+        self.layout.addWidget(self.fc.widget(), 0, 0, 2, 1)
+
+        self.configNodes()
+        self.configScatterPlot()
+
+        self.getWiimote()
+
+        self.pathX = []
+        self.pathY = []
+
+        self.isPathCollected = False
+        self.printedPath = False
+
+    # connect to wiimote and config wiimote node
+    def getWiimote(self):
+        if len(sys.argv) == 1:
+            addr, name = wiimote.find()[0]
+        elif len(sys.argv) == 2:
+            addr = sys.argv[1]
+            name = None
+        elif len(sys.argv) == 3:
+            addr, name = sys.argv[1:3]
+        print("Connecting to %s (%s)" % (name, addr))
+
+        self.wiimoteNode.text.setText(addr)
+        self.wiimoteNode.connect_wiimote()
+
+    # create and connect nodes
+    def configNodes(self):
+        self.pointVisNode = self.fc.createNode('Vis2D', pos=(-150, 150))
+        self.wiimoteNode = self.fc.createNode('Wiimote', pos=(0, 0), )
+        self.bufferNode = self.fc.createNode('Buffer', pos=(0, -150))
+
+        self.buffer_amount = self.bufferNode.getBufferSize()
+
+        self.fc.connectTerminals(
+            self.wiimoteNode['irVals'],
+            self.bufferNode['dataIn'])
+        self.fc.connectTerminals(
+            self.bufferNode['dataOut'],
+            self.pointVisNode['irVals'])
+
+    # create and config scatter plot item
+    def configScatterPlot(self):
+        gview = pg.GraphicsLayoutWidget()
+        self.layout.addWidget(gview, 0, 1, 2, 1)
+        plot = gview.addPlot()
+        self.scatter = pg.ScatterPlotItem(
+            size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 255, 255, 120))
+        plot.addItem(self.scatter)
+        #plot.setXRange(-1000, 200)
+        #plot.setYRange(-1000, 200)
+
+    def keyPressEvent(self, ev):
+        if ev.key() == QtCore.Qt.Key_Escape:
+            self.close()
+
+    # do actions in loop
+    def update(self):
+        outputValues = self.pointVisNode.outputValues()
+
+        if outputValues['irX'] is not None and outputValues['irY'] is not None:
+            irX = outputValues['irX']
+            irY = outputValues['irY']
+
+            # raise or lower buffer amount with +/- keys
+            if self.wiimoteNode.wiimote is not None:
+                """
+                if self.wiimoteNode.wiimote.buttons['Plus']:
+                    self.buffer_amount += 1
+                    self.bufferNode.setBufferSize(self.buffer_amount)
+                elif self.wiimoteNode.wiimote.buttons['Minus']:
+                    if self.buffer_amount > 1:
+                        self.buffer_amount -= 1
+                        self.bufferNode.setBufferSize(self.buffer_amount)
+                """
+                if self.wiimoteNode.wiimote.buttons['A']:
+                    self.scatter.clear()
+                    self.pathX.append(irX)
+                    self.pathY.append(irY)
+                elif self.pathX is not None and len(self.pathX) > 0:
+                    mergedValues = []
+                    for i in range(0, len(self.pathX)):
+                        mergedValues.append([self.pathX[i], self.pathY[i]])
+                    self.scatter.setData(pos=np.array(mergedValues))
+                    self.pathX = []
+                    self.pathY = []
+
+        pyqtgraph.QtGui.QApplication.processEvents()
+
+if __name__ == "__main__":
+    main()
