6b217acdd74a3d3a33cf4c729f696eedee042d7c
10freefingers <10freefingers@gmail.com>
2014-06-26 12:55:51 +0200

    Refactoring, adding comments

--- drill8_path/path.py
+++ drill8_path/path.py
@@ -77,9 +77,10 @@ class Demo(QtGui.QWidget):
     light source detected by the wiimote
     '''
     def config_nodes(self):
-        self.pointVisNode = self.fc.createNode('Vis2D', pos=(-150, 150))
         self.wiimoteNode = self.fc.createNode('Wiimote', pos=(0, 0), )
         self.bufferNode = self.fc.createNode('Buffer', pos=(0, -150))
+        self.pointVisNode = self.fc.createNode('Vis2D', pos=(-150, 150))
+
         self.bufferNode.setBufferSize(128)
 
         self.fc.connectTerminals(
@@ -100,7 +101,6 @@ class Demo(QtGui.QWidget):
         self.scatter = pg.ScatterPlotItem(
             size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 255, 255, 120))
         plot.addItem(self.scatter)
-        #plot.enableAutoRange()
         plot.setXRange(-400, 1400)
         plot.setYRange(-400, 1400)
 
@@ -118,18 +118,26 @@ class Demo(QtGui.QWidget):
         circlePoints = [(269, 84), (263, 86), (257, 92), (253, 98), (249, 104), (245, 114), (243, 122), (239, 132), (237, 142), (235, 152), (235, 162), (235, 172), (235, 180), (239, 190), (245, 198), (251, 206), (259, 212), (267, 216), (275, 218), (281, 222), (287, 224), (295, 224), (301, 226), (311, 226), (319, 226), (329, 226), (339, 226), (349, 226), (352, 226), (360, 226), (362, 225), (366, 219), (367, 217), (367, 209), (367, 206), (367, 198), (367, 190), (367, 182), (367, 174), (365, 166), (363, 158), (359, 152), (355, 146), (353, 138), (349, 134), (345, 130), (341, 124), (340, 122), (338, 121), (337, 119), (336, 117), (334, 116), (332, 115), (331, 114), (327, 110), (325, 109), (323, 109), (321, 108), (320, 108), (318, 107), (316, 107), (315, 107), (314, 107), (313, 107), (312, 107), (311, 107), (310, 107), (309, 106), (308, 106), (307, 105), (306, 105), (305, 105), (304, 105), (303, 104), (302, 104), (301, 104), (300, 104), (299, 103), (298, 103), (296, 102), (295, 101), (293, 101), (292, 100), (291, 100), (290, 100), (289, 100), (288, 100), (288, 99), (287, 99), (287, 99)]
         squarePoints = [(193, 123), (193, 131), (193, 139), (195, 151), (197, 161), (199, 175), (201, 187), (205, 201), (207, 213), (209, 225), (213, 235), (213, 243), (215, 251), (215, 254), (217, 262), (217, 264), (217, 266), (217, 267), (218, 267), (219, 267), (221, 267), (224, 267), (227, 267), (237, 267), (247, 265), (259, 263), (273, 261), (287, 261), (303, 259), (317, 257), (331, 255), (347, 255), (361, 253), (375, 253), (385, 253), (395, 251), (403, 249), (406, 249), (408, 249), (408, 248), (409, 248), (409, 246), (409, 245), (409, 242), (409, 234), (409, 226), (409, 216), (407, 204), (407, 194), (405, 182), (403, 172), (403, 160), (401, 150), (399, 140), (399, 130), (397, 122), (397, 119), (397, 116), (396, 114), (396, 112), (396, 111), (396, 110), (396, 109), (396, 108), (396, 107), (396, 106), (396, 105), (394, 105), (392, 105), (384, 105), (376, 105), (364, 105), (350, 107), (334, 109), (318, 111), (306, 113), (294, 115), (286, 117), (278, 117), (272, 119), (269, 119), (263, 121), (260, 121), (254, 123), (251, 123), (245, 125), (243, 125), (242, 125), (241, 126), (240, 126), (238, 127), (236, 127), (232, 128), (231, 128), (231, 129), (230, 129), (228, 129), (227, 129), (226, 129), (225, 129), (224, 129), (223, 129), (222, 129), (221, 130), (221, 130)]
         trianglePoints = [(282, 83), (281, 85), (277, 91), (273, 97), (267, 105), (261, 113), (253, 123), (243, 133), (235, 141), (229, 149), (221, 153), (217, 159), (216, 160), (215, 161), (214, 162), (216, 162), (218, 162), (221, 162), (227, 164), (233, 166), (241, 166), (249, 166), (259, 166), (271, 166), (283, 166), (297, 166), (309, 164), (323, 164), (335, 162), (345, 162), (353, 162), (361, 160), (363, 159), (365, 159), (366, 158), (367, 158), (368, 157), (369, 157), (370, 156), (371, 156), (371, 155), (372, 155), (372, 153), (372, 152), (372, 151), (372, 149), (372, 147), (371, 145), (367, 141), (363, 137), (359, 133), (353, 129), (349, 125), (343, 121), (337, 119), (333, 115), (327, 111), (325, 110), (324, 109), (320, 105), (318, 104), (314, 100), (312, 99), (310, 98), (306, 94), (305, 93), (303, 92), (301, 91), (300, 90), (298, 89), (297, 88), (296, 88), (295, 87), (294, 87), (293, 87), (293, 87)]
+
+        #self.scatter.addPoints(pos=np.array(circlePoints), brush=pg.mkBrush(0, 255, 0, 120))
+        #self.scatter.addPoints(pos=np.array(squarePoints), brush=pg.mkBrush(0, 255, 0, 120))
+        #self.scatter.addPoints(pos=np.array(trianglePoints), brush=pg.mkBrush(0, 255, 0, 120))
+
         self.dollar.addTemplate('circle', circlePoints)
         self.dollar.addTemplate('square', squarePoints)
         self.dollar.addTemplate('triangle', trianglePoints)
 
     def update(self):
+        # get biggest light's x/y values
         outputValues = self.pointVisNode.outputValues()
         if outputValues['irX'] is not None and outputValues['irY'] is not None:
             if self.wiimoteNode.wiimote is not None:
                 if self.wiimoteNode.wiimote.buttons['A']:
+                    # collect values and set state
                     self.construct_path(outputValues)
                     self.pressed_key = 'A'
                 elif self.wiimoteNode.wiimote.buttons['B']:
+                    # collect values and set state
                     self.construct_path(outputValues)
                     self.pressed_key = 'B'
                 elif self.path['x'] is not None and len(self.path['x']) > 0:
@@ -147,29 +155,45 @@ class Demo(QtGui.QWidget):
     '''
     def create_template(self):
         points = []
+
+        # combine x/y path arrays to one point array
         for i in range(0, len(self.path['x'])):
             points.append([self.path['x'][i], self.path['y'][i]])
-        name = 'tpl_' + str((len(self.dollar.templates) + 1))
-        self.dollar.addTemplate(name, circle_points)
+
+        # avoid devision by zero
+        if len(points) > 0:
+            # name and add template
+            name = 'tpl_' + str((len(self.dollar.templates) + 1))
+            self.dollar.addTemplate(name, points)
 
     '''
     The user input is compared to all available templates and depending
     on the accordance a text message is displayed
     '''
     def compare_template(self):
-        points = []
-        for i in range(0, len(self.path['x'])):
-            points.append([self.path['x'][i], self.path['y'][i]])
+        points = self.combineXYPoints()
+
+        # try recognizing points. get name of template that matches most
+        # and its amount of matching
         name, score = self.dollar.recognize(points)
+
         score = score * 100
         if score > self.threshold:
+            # template matches good enough
             self.display_message(name)
+
+            # get template by name
             template = [t for t in self.dollar.templates if t.name == name][0]
+
             tpl_points = []
+
+            # collect and display template points
             for i in range(0, len(template.points)):
                 tpl_points.append([template.points[i].x, template.points[i].y])
+            # display points
             self.scatter.addPoints(pos=np.array(tpl_points), brush=pg.mkBrush(0, 255, 0, 120))
         else:
+            # template doesn't match good enough
             self.display_message(self.default_msg)
 
     '''
@@ -184,18 +208,29 @@ class Demo(QtGui.QWidget):
     The stored infrafred values are passed to a scatterplot
     '''
     def draw_path(self):
-        points = []
-        for i in range(0, len(self.path['x'])):
-            points.append([self.path['x'][i], self.path['y'][i]])
-        #self.scatter.setData(pos=np.array(points))
+        points = self.combineXYPoints()
+
+        # display points
         self.scatter.addPoints(pos=np.array(points), brush=pg.mkBrush(255, 255, 255, 120))
+
+        # handle pressed keys
         if self.pressed_key is 'A':
             self.compare_template()
         elif self.pressed_key is 'B':
             self.create_template()
+
         self.path['x'] = []
         self.path['y'] = []
         self.pressed_key = None
+
+    '''
+    Combine separate x and y point arrays to one nested array
+    '''
+    def combineXYPoints(self):
+        points = []
+        for i in range(0, len(self.path['x'])):
+            points.append([self.path['x'][i], self.path['y'][i]])
+        return points
 
     '''
     A text message is passed to a ui label widget
